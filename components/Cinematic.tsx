0200bcbf3fae8396e24ed970e9d45aab6d22c718
Aziz-Rabya <azizrabya81@gmail.com>
2026-07-16 22:23:23 +0100

    cinematic section

--- components/Cinematic.tsx
+++ components/Cinematic.tsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 const Cinematic = () => {
   return (
-    <div>Cinematic</div>
+    <div className="relative h-screen w-full bg-cover bg-center overflow-hidden">
+      <div>
+        <video
+          src="/videos/house.mp4"
+          autoPlay
+          loop
+          muted
+          playsInline
+          preload="auto"
+          className="h-full w-full object-cover"
+        />
+      </div>
+      <div>
+        <h1 className="absolute phoenix top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-9xl font-bold">
+          THE WORLD IS YOURS      </h1>
+      </div>
+    </div>
   )
 }
 
